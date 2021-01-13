995d377691337c8b6aea57f1e5f347e9ff4cd574
Mark Pope <itsmarkpope@gmail.com>
2021-01-13 06:34:59 -0800

    Adding more drawing functions

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -1,6 +1,7 @@
 #include "Main.h"
 #include "Helpers.h"
 
+// Define variables which determine how big the window will be
 int SCREEN_WIDTH = 800;
 int SCREEN_HEIGHT = 600;
 
@@ -16,6 +17,15 @@ void GameInit()
 
 void GameLoop()
 {
-    // Create a circle shape
-    DrawCircle(50, 25, 25, sf::Color::Green);
+    // Draw a circle
+    DrawCircle(100, 50, 50, sf::Color::Green);
+
+    // Draw a rectangle
+    DrawRectangle(130, 60, 200, 100, sf::Color::Magenta);
+
+    // Draw a pixel
+    DrawPixel(30, 180, sf::Color::Yellow);
+
+    // Draw a line
+    DrawLine(230, 80, 100, 400, sf::Color::Cyan);
 }
--- Game/Game/Helpers.cpp
+++ Game/Game/Helpers.cpp
@@ -6,12 +6,57 @@
 
 void DrawCircle(float centerX, float centerY, float radius, sf::Color color)
 {
+    // Create a circle object
 	sf::CircleShape myCircle(radius);
+
+    // Set the top left position by subracting the radius from the center position
     float left = centerX - radius;
     float top = centerY - radius;
     myCircle.setPosition(left, top);
+
+    // Set the color
 	myCircle.setFillColor(color);
+
+    // Draw it
     window->draw(myCircle);
+}
+
+void DrawRectangle(float left, float top, float width, float height, sf::Color color)
+{
+    // Create a rectangle object
+    sf::RectangleShape rectangle(sf::Vector2f(width, height));
+
+    // Set the top left position
+    rectangle.setPosition(left, top);
+
+    // Set the color
+    rectangle.setFillColor(color);
+
+    // Draw it
+    window->draw(rectangle);
+}
+
+void DrawPixel(float x, float y, sf::Color color)
+{
+    // Draw a 1x1 rectangle, using the DrawRectangle function above
+    DrawRectangle(x, y, 1, 1, color);
+}
+
+void DrawLine(float x1, float y1, float x2, float y2, sf::Color color)
+{
+    // Make an array of the two end points (an array is like a list)
+    sf::Vertex points[] =
+    {
+        sf::Vertex(sf::Vector2f(x1, y1)),
+        sf::Vertex(sf::Vector2f(x2, y2))
+    };
+
+    // Set the color of the points
+    points[0].color = color;
+    points[1].color = color;
+
+    // Draw the line made by the two end points
+    window->draw(points, 2, sf::Lines);
 }
 
 
@@ -21,13 +66,16 @@ void DrawCircle(float centerX, float centerY, float radius, sf::Color color)
 // about understanding it :)
 int __cdecl printf2(const char* format, ...)
 {
+    // Create temporary storage for the text
     char str[1024];
 
+    // Build the text into str
     va_list argptr;
     va_start(argptr, format);
     int ret = vsnprintf(str, sizeof(str), format, argptr);
     va_end(argptr);
 
+    // Print the string to Visual Studio
     OutputDebugStringA(str);
 
     return ret;
